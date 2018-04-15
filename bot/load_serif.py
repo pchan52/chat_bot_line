e061318a074231558aa671b6f7a9e89f0c60b398
pchan52 <pchan52611625@gmail.com>
2018-04-15 15:35:36 +0900

    first commit

---
+++ bot/load_serif.py
@@ -0,0 +1,1 @@
+osomatsu_serif = open("osomatsu_serif.txt").read().split("\n")
---
+++ bot/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+import json
+import random
+import requests
+from load_serif import osomatsu_serif
+
+REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
+ACCESS_TOKEN = 's1QnhL2/aBnNeuGpbaQMgzCTw0jSs3d8Yun+CXQs0mjX/Z60qiKcuNn2N76OjWVydkXuez9o4Dc8s9zLKZTwwsNbyYB9IjKg9joidEVTpeQe7jTTevQ/90zrrr4D6HfJiM6zHPT6lX/zULN9m0BlrgdB04t89/1O/w1cDnyilFU='
+HEADER = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + ACCESS_TOKEN
+}
+
+def reply_text(reply_token, text):
+    reply = random.choice(osomatsu_serif)
+    payload = {
+          "replyToken":reply_token,
+          "messages":[
+                {
+                    "type":"text",
+                    "text": reply
+                }
+            ]
+    }
+
+    requests.post(REPLY_ENDPOINT, headers=HEADER, data=json.dumps(payload))
+    return reply
+
+
+def index(request):
+    return HttpResponse("this is bot api")
+
+
+def callback(request):
+    reply = ""
+    request_json = json.loads(request.body.decode('utf-8'))
+    for e in request_json['events']:
+        reply_token = e['replyToken']
+        message_type = e['message']['type']
+
+        if message_type == 'text':
+            text = e['message']['text']
+            reply += reply_text(reply_token, text)
+    return HttpResponse(reply)
